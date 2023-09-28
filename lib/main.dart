0f0cd003a2569634495999798e28cbaec16df42d
Hideaki Takahashi <hi_takahashi@yumemi.co.jp>
2023-09-28 22:36:56 +0900

    feat: setting ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'package:socket_io_demo/router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
