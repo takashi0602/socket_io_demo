349c72aa0bcabca11a4ad044d33ba1fed52d3e45
Hideaki Takahashi <hi_takahashi@yumemi.co.jp>
2023-09-29 02:22:11 +0900

    feat: use roomsAsyncNotifierProvider

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:socket_io_demo/features/room/api/rooms.dart';
 import 'package:socket_io_demo/pages/room_page.dart';
 
 part 'top_page.g.dart';
@@ -24,16 +26,54 @@ class TopPage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Flutter x Socket.io Demo'),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            TextButton(
-              onPressed: () => const RoomPageRoute(id: 1).push(context),
-              child: const Text('ルーム1へ'),
-            ),
-          ],
-        ),
-      ),
+      body: const _Body(),
+    );
+  }
+}
+
+class _Body extends ConsumerWidget {
+  const _Body();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final roomsAsyncNotifier = ref.watch(roomsAsyncNotifierProvider);
+    return roomsAsyncNotifier.when(
+      data: (rooms) {
+        return ListView.builder(
+          itemCount: rooms.length,
+          itemBuilder: (context, index) {
+            final room = rooms[index];
+
+            return ListTile(
+              title: Text('Room ${room.id}'),
+              trailing: const Icon(Icons.navigate_next),
+              onTap: () {
+                RoomPageRoute(id: room.id).push(context);
+              },
+            );
+          },
+        );
+      },
+      error: (_, __) {
+        return const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Error😢'),
+            ],
+          ),
+        );
+      },
+      loading: () {
+        return const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(),
+            ],
+          ),
+        );
+      },
     );
   }
 }
