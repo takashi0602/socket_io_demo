1a9a430ea2a51e39f7cc03c2343277902b6a458c
takashi0602 <t.takashi0602@gmail.com>
2023-10-09 23:11:16 +0900

    refactor: delete unnecessary widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'package:socket_io_demo/router.dart';
 
 void main() {
@@ -19,57 +18,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       routerConfig: router,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final IO.Socket _socket = IO.io(
-    "http://localhost:3000",
-    IO.OptionBuilder()
-        .setTransports(['websocket'])
-        .disableAutoConnect()
-        .build(),
-  );
-
-  @override
-  void initState() {
-    super.initState();
-
-    _socket.onConnect((_) => print('connect'));
-    _socket.onDisconnect((_) => print('disconnect'));
-    // TODO: メッセージのやり取り
-    // TODO: user idはString.fromEnviromentで取得する（flutter run時にU--dart-define=SER_ID=〇〇をセットする）
-    _socket.connect();
-  }
-
-  @override
-  void dispose() {
-    _socket.disconnect();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Column(
-          children: [],
-        ),
-      ),
     );
   }
 }
